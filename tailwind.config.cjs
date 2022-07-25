0dbd175e5fcdf3139dd7c0f026ee409a470a77e5
Josh Searle <joshryansearle@gmail.com>
2022-07-26 07:43:11 +1000

    build: add tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/forms")],
+}
