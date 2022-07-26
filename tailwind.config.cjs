39c8b81d85c39fe81092c898c09dc99f0a80e7cf
Josh Searle <joshryansearle@gmail.com>
2022-07-26 21:17:06 +1000

    feat: configure default colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,20 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
+const colors = require("tailwindcss/colors")
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: colors.zinc["900"],
+        default: colors.zinc["100"],
+        border: colors.zinc["600"],
+        shadow: "#101012",
+        accent: colors.cyan["900"],
+      },
+    },
   },
   plugins: [require("@tailwindcss/forms")],
 }
