d2fdc2e6111e7d613a68da74e5e315809dcebed8
Josh Searle <joshryansearle@gmail.com>
2022-07-26 21:18:46 +1000

    feat: add input and button components

---
+++ src/components/textarea-input.tsx
@@ -0,0 +1,29 @@
+import throughEvent from "../utils/throughEvent"
+
+const TextareaInput = ({
+  label,
+  htmlFor,
+  value,
+  changeHandler,
+}: {
+  label: string
+  htmlFor: string
+  value: string
+  changeHandler: (_: string) => void
+}) => {
+  return (
+    <div className="flex flex-col">
+      <label htmlFor={htmlFor} className="font-semibold">
+        {label}
+      </label>
+      <textarea
+        id={htmlFor}
+        value={value}
+        onChange={throughEvent(changeHandler)}
+        className="bg-background border border-border rounded-md shadow-sm shadow-shadow px-3.5 py-2 text-lg focus-visible:outline-none outline-none focus-visible:ring-2 focus-visible:ring-cyan-900 focus-visible:border-transparent duration-[50ms]"
+      ></textarea>
+    </div>
+  )
+}
+
+export default TextareaInput
