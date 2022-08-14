6e34043d8fb4652b11a35778d99a38ad3dc6c5a4
Josh Searle <joshryansearle@gmail.com>
2022-08-14 16:25:33 +1000

    feat: change to page states and add set form

--- src/pages/exercise.tsx
+++ src/pages/exercise.tsx
@@ -1,11 +1,12 @@
 import * as AlertDialog from "@radix-ui/react-alert-dialog"
 import { useLiveQuery } from "dexie-react-hooks"
-import { useCallback, useEffect, useMemo, useRef, useState } from "react"
+import { memo, useCallback, useEffect, useRef, useState } from "react"
 import toast from "react-hot-toast"
 import { useNavigate, useParams } from "react-router-dom"
 import Button from "../components/button"
-import TextInput from "../components/text-input"
-import TextareaInput from "../components/textarea-input"
+import DateInput from "../components/date-input"
+import ExerciseForm from "../components/exercise-form"
+import TimeInput from "../components/time-input"
 import { db, Exercise } from "../data/db"
 
 const useExercise = (id: string | undefined) => {
@@ -19,50 +20,40 @@ const useUpdateExercise = (
   exercise: Exercise | undefined,
   noUpdate: () => void
 ) => {
-  const [name, setName] = useState("")
-  const [notes, setNotes] = useState("")
+  const updateExercise = useCallback(
+    async ({ name, notes }: { name: string; notes: string }) => {
+      if (name === exercise?.name && notes === exercise.notes) {
+        toast.success("Exercise updated")
+        noUpdate()
+        return
+      }
 
-  useEffect(() => {
-    setName(exercise?.name ? exercise.name : "")
-    setNotes(exercise?.notes ? exercise.notes : "")
-  }, [exercise])
-
-  const valid = name.length !== 0 && !!exercise
-
-  const updateExercise = useCallback(async () => {
-    if (name === exercise?.name && notes === exercise.notes) {
-      toast.success("Exercise updated")
-      noUpdate()
-      return
-    }
-
-    if (!exercise) {
-      toast.error("Failed to update exercise")
-      return
-    }
-
-    const sameName = await db.exercises.where({ name }).toArray()
-
-    if (sameName.length !== 0 && name !== exercise.name) {
-      toast.error("Exercise with that name already exists")
-      return
-    }
-
-    db.exercises
-      .update(exercise.id, { name, notes })
-      .then((updatedCount) => {
-        if (updatedCount === 0) throw new Error("Failed to update exercise")
-        else toast.success("Exercise updated")
-      })
-      .catch(() => {
+      if (!exercise) {
         toast.error("Failed to update exercise")
-      })
-  }, [exercise, name, notes])
+        return
+      }
 
-  return useMemo(
-    () => ({ name, setName, notes, setNotes, valid, updateExercise }),
-    [name, notes, updateExercise]
+      const sameName = await db.exercises.where({ name }).toArray()
+
+      if (sameName.length !== 0 && name !== exercise.name) {
+        toast.error("Exercise with that name already exists")
+        return
+      }
+
+      db.exercises
+        .update(exercise.id, { name, notes })
+        .then((updatedCount) => {
+          if (updatedCount === 0) throw new Error("Failed to update exercise")
+          else toast.success("Exercise updated")
+        })
+        .catch(() => {
+          toast.error("Failed to update exercise")
+        })
+    },
+    [exercise]
   )
+
+  return updateExercise
 }
 
 const useDeleteExercise = (onSuccess: () => void) => {
@@ -79,23 +70,63 @@ const useDeleteExercise = (onSuccess: () => void) => {
   }, [])
 }
 
+const SetForm = memo(
+  (x: {
+    submitMessage: string
+    onSubmit: ({ name, notes }: { name: string; notes: string }) => void
+    closeForm: () => void
+  }) => {
+    console.log(x)
+
+    return (
+      <div className="border border-border rounded-md shadow-shadow p-4">
+        <div className="flex flex-row justify-between space-x-4">
+          <div className="w-full">
+            <DateInput
+              label="Date"
+              htmlFor="setDate"
+              value={""}
+              changeHandler={() => null}
+            />
+          </div>
+          <div className="w-full">
+            <TimeInput
+              label="Time"
+              htmlFor="setTime"
+              value={""}
+              changeHandler={() => null}
+            />
+          </div>
+        </div>
+      </div>
+    )
+  }
+)
+
+enum PageState {
+  NORMAL,
+  EDITING_EXERCISE,
+  CREATING_SET,
+  EDITTING_SET,
+}
+
 const ExercisePage = () => {
-  const [editing, setEditing] = useState(false)
+  const [pageState, setPageState] = useState<PageState>(PageState.NORMAL)
 
   const { id } = useParams<{ id: string }>()
 
   const exercise = useExercise(id)
-  const { name, setName, notes, setNotes, updateExercise } = useUpdateExercise(
-    exercise,
-    () => setEditing(false)
+  const updateExercise = useUpdateExercise(exercise, () =>
+    setPageState(PageState.NORMAL)
   )
   const navigate = useNavigate()
   const deleteExercise = useDeleteExercise(() => navigate("/exercises"))
 
   const firstEditingRenderDone = useRef(false)
 
+  // Set page state to normal when live query updates
   useEffect(() => {
-    if (!editing) {
+    if (pageState !== PageState.EDITING_EXERCISE) {
       firstEditingRenderDone.current = false
       return
     }
@@ -103,42 +134,39 @@ const ExercisePage = () => {
     if (!firstEditingRenderDone.current) {
       firstEditingRenderDone.current = true
     } else {
-      setEditing(false)
+      setPageState(PageState.NORMAL)
     }
-  }, [editing, exercise?.name, exercise?.notes])
+  }, [pageState, exercise?.name, exercise?.notes])
 
   if (!exercise) return null
 
   return (
     <div className="space-y-4">
-      {!editing ? (
+      {pageState === PageState.EDITING_EXERCISE && (
+        <ExerciseForm
+          submitMessage="Save"
+          onSubmit={updateExercise}
+          closeForm={() => setPageState(PageState.NORMAL)}
+          initialName={exercise.name}
+          initialNotes={exercise.notes}
+        />
+      )}
+      {pageState !== PageState.EDITING_EXERCISE && (
         <h2 className="font-semibold text-xl">{exercise.name}</h2>
-      ) : (
-        <TextInput
-          label="Exercise name"
-          value={name}
-          changeHandler={setName}
-          htmlFor="exerciseName"
-        />
       )}
-      {!editing ? (
-        !!exercise.notes && (
-          <div className="whitespace-pre border border-border rounded-md shadow-sm shadow-shadow p-3.5 py-2.5">
-            {exercise.notes}
-          </div>
-        )
-      ) : (
-        <TextareaInput
-          label="Exercise notes"
-          value={notes}
-          changeHandler={setNotes}
-          htmlFor="exerciseNotes"
-        />
+      {pageState !== PageState.EDITING_EXERCISE && !!exercise.notes && (
+        <div className="whitespace-pre border border-border rounded-md shadow-sm shadow-shadow p-3.5 py-2.5">
+          {exercise.notes}
+        </div>
       )}
-      {!editing ? (
+      {pageState === PageState.NORMAL && (
         <div className="space-x-4">
-          <Button onClick={() => null}>Log set</Button>
-          <Button onClick={() => setEditing((b) => !b)}>Edit</Button>
+          <Button onClick={() => setPageState(PageState.CREATING_SET)}>
+            Log set
+          </Button>
+          <Button onClick={() => setPageState(PageState.EDITING_EXERCISE)}>
+            Edit
+          </Button>
           <AlertDialog.Root>
             <AlertDialog.Trigger asChild>
               <Button onClick={() => null}>Delete</Button>
@@ -171,14 +199,16 @@ const ExercisePage = () => {
             </AlertDialog.Content>
           </AlertDialog.Root>
         </div>
-      ) : (
-        <div className="space-x-4">
-          <Button onClick={updateExercise}>Save</Button>
-          <Button onClick={() => setEditing(false)}>Cancel</Button>
-        </div>
+      )}
+      {pageState === PageState.CREATING_SET && (
+        <SetForm
+          submitMessage="Log"
+          onSubmit={() => null}
+          closeForm={() => setPageState(PageState.NORMAL)}
+        />
       )}
     </div>
   )
 }
 
-export default ExercisePage
+export default memo(ExercisePage)
