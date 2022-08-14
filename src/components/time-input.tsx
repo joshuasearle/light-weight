5b46c36df3924881d46f1f425197a57b3edd4deb
Josh Searle <joshryansearle@gmail.com>
2022-08-14 16:25:15 +1000

    feat: create date and time inputs

---
+++ src/components/time-input.tsx
@@ -0,0 +1,31 @@
+import { memo } from "react"
+import throughEvent from "../utils/through-event"
+
+const TimeInput = ({
+  label,
+  htmlFor,
+  value,
+  changeHandler,
+}: {
+  label: string
+  htmlFor: string
+  value: string
+  changeHandler: (_: string) => void
+}) => {
+  return (
+    <div className="flex flex-col">
+      <label htmlFor={htmlFor} className="font-semibold">
+        {label}
+      </label>
+      <input
+        type="time"
+        id={htmlFor}
+        value={value}
+        onChange={throughEvent(changeHandler)}
+        className="w-full text-default bg-background border border-border rounded-md shadow-sm shadow-shadow px-3.5 py-2 text-xs xs:text-base sm:text-lg outline-none focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-cyan-900 focus-visible:border-transparent duration-[50ms]"
+      />
+    </div>
+  )
+}
+
+export default memo(TimeInput)
