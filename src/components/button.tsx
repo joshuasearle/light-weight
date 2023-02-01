c4b4ca0c0022de81e56534aa304a88f5ce767234
Josh Searle <joshryansearle@gmail.com>
2023-02-01 22:23:50 +1100

    Add button type to prevent page refresh

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -3,14 +3,17 @@ import { memo, ReactNode } from "react"
 const Button = ({
   children,
   onClick,
+  type = "button",
 }: {
   children: ReactNode
   onClick: () => void
+  type?: "button" | "submit"
 }) => {
   return (
     <button
       className="text-sm sm:text-base sm:px-3.5 sm:py-2 px-3 py-1.5 border border-border rounded-md shadow-sm shadow-shadow font-semibold outline-none focus-visible:ring-2 ring-cyan-900 duration[50ms] focus-visible:border-transparent"
       onClick={onClick}
+      type={type}
     >
       {children}
     </button>
